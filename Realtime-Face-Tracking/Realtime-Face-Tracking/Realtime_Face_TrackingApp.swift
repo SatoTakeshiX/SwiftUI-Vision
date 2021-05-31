65db1bfe8b41800e066e4acb5399040958d4b8ce
satoutakeshi <sato.takeshi1987@gmail.com>
2021-05-31 18:08:01 +0900

    add realtime-face-tracking project

---
+++ Realtime-Face-Tracking/Realtime-Face-Tracking/Realtime_Face_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realtime_Face_TrackingApp.swift
+//  Realtime-Face-Tracking
+//
+//  Created by satoutakeshi on 2021/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Realtime_Face_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
