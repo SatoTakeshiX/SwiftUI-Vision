64fec03da3d0e1d0a315f2f462fb58e0594e7484
satoutakeshi <sato.takeshi1987@gmail.com>
2021-06-21 22:40:47 +0900

    add makeFaceFeaturesPoints

--- Detected-in-Still-Image/Detected-in-Still-Image/Model/VisionClient.swift
+++ Detected-in-Still-Image/Detected-in-Still-Image/Model/VisionClient.swift
@@ -10,8 +10,8 @@ import Vision
 
 enum VisionRequestTypes {
     case unknown
-    case faceRect(rectBox: CGRect)
-    case faceLandmarks(observation: [VNFaceObservation])
+    case faceRect(rectBox: [CGRect])
+    case faceLandmarks(drawPoints: [[Bool: [CGPoint]]])
     case text
     case barcode
     case rect
@@ -44,16 +44,19 @@ final class VisionClient: ObservableObject {
 
     /// ensure in main thread when you use the value
     @Published var result: VisionRequestTypes = .unknown
+    @Published var error: Error?
 
-    func configure(_ requests: VisionRequestTypes.Set, imageViewFrame: CGRect) {
-        self.requestTypes = requests
+    func request(type: VisionRequestTypes.Set, imageViewFrame: CGRect) {
+        self.requestTypes = type
         self.imageViewFrame = imageViewFrame
     }
 
     // MARK: Vision Requests
-    lazy var faceDetectionRequest = VNDetectFaceRectanglesRequest(completionHandler: { request, error in
+    lazy var faceDetectionRequest = VNDetectFaceRectanglesRequest(completionHandler: { [weak self] request, error in
+        guard let self = self else { return }
         if let error = error {
             print(error.localizedDescription)
+            self.error = error
             return
         }
 
@@ -61,10 +64,16 @@ final class VisionClient: ObservableObject {
             return
         }
 
-
+        let rectBoxs = results.map { observation -> CGRect in
+            let rectBox = self.boundingBox(forRegionOfInterest: observation.boundingBox, withinImageBounds: self.imageViewFrame)
+            print("detected Rect: \(rectBox.debugDescription)")
+            return rectBox
+        }
+        self.result = .faceRect(rectBox: rectBoxs)
     })
 
-    lazy var faceLandmarkRequest = VNDetectFaceLandmarksRequest(completionHandler: { request, error in
+    lazy var faceLandmarkRequest = VNDetectFaceLandmarksRequest(completionHandler: { [weak self] request, error in
+        guard let self = self else { return }
         if let error = error {
             print(error.localizedDescription)
             return
@@ -73,8 +82,8 @@ final class VisionClient: ObservableObject {
         guard let results = request.results as? [VNFaceObservation] else {
             return
         }
-        // need to use in main thread
-        self.result = .faceLandmarks(observation: results)
+        let points = self.makeFaceFeaturesPoints(onFaces: results, onImageWithBounds: self.imageViewFrame)
+        self.result = .faceLandmarks(drawPoints: points)
     })
 
     func boundingBox(forRegionOfInterest: CGRect,
@@ -97,5 +106,65 @@ final class VisionClient: ObservableObject {
         rect.size.height *= imageHeight
 
         return rect
+    }
+
+    private func makeFaceFeaturesPoints(onFaces faces: [VNFaceObservation], onImageWithBounds bounds: CGRect) -> [[Bool: [CGPoint]]] {
+        var landmarkPoints: [[Bool: [CGPoint]]] = []
+        
+        for face in faces {
+            let faceBounds = boundingBox(forRegionOfInterest: face.boundingBox, withinImageBounds: bounds)
+            guard let landmarks = face.landmarks else {
+                return []
+            }
+
+            let openLandmarkRegions = [
+                landmarks.leftEyebrow,
+                landmarks.rightEyebrow,
+                landmarks.faceContour,
+                landmarks.noseCrest,
+                landmarks.medianLine
+            ].compactMap { $0 }
+
+            // Draw eyes, lips, and nose as closed regions.
+            let closedLandmarkRegions = [
+                landmarks.leftEye,
+                landmarks.rightEye,
+                landmarks.outerLips,
+                landmarks.innerLips,
+                landmarks.nose
+                ].compactMap { $0 }
+
+            let openLandmarkPoints = openLandmarkRegions.compactMap { region -> [Bool: [CGPoint]]? in
+                guard let points = self.makeNormalizedPoints(region: region, faceBounds: faceBounds) else {
+                    return nil
+                }
+                return [false: points]
+            }
+
+            landmarkPoints += openLandmarkPoints
+
+            let closedLandmarksPoints = closedLandmarkRegions.compactMap { region -> [Bool: [CGPoint]]? in
+                guard let points = self.makeNormalizedPoints(region: region, faceBounds: faceBounds) else {
+                    return nil
+                }
+                return [true: points]
+            }
+
+            landmarkPoints += closedLandmarksPoints
+        }
+
+        return landmarkPoints
+    }
+
+    private func makeNormalizedPoints(region: VNFaceLandmarkRegion2D, faceBounds: CGRect) -> [CGPoint]? {
+        guard region.pointCount > 1 else {
+            return nil
+        }
+        let points = region.normalizedPoints.map { point -> CGPoint in
+            let x = point.x * faceBounds.width + faceBounds.origin.x
+            let y = point.y * faceBounds.height + faceBounds.origin.y
+            return CGPoint(x: x, y: y)
+        }
+        return points
     }
 }
