a3da4ae51d9cbec0f61141a60b857f63f8eb0b67
satoutakeshi <sato.takeshi1987@gmail.com>
2021-07-02 01:11:46 +0900

    resize method become simple

--- Detected-in-Still-Image/Detected-in-Still-Image/Scene/DetectorViewModel.swift
+++ Detected-in-Still-Image/Detected-in-Still-Image/Scene/DetectorViewModel.swift
@@ -84,10 +84,10 @@ final class DetectorViewModel: ObservableObject {
 
     func onAppear(image: UIImage, detectType: VisionRequestTypes.Set) {
         self.image = image
-        let correctedImage = scaleAndOrient(image: image)
-        print(correctedImage.description) // "<UIImage:0x600003e0c240 anonymous {640, 394}>"
+        guard let resizedImage = resize(image: image) else { return }
+        print(resizedImage.description)
         // Transform image to fit screen.
-        guard let cgImage = correctedImage.cgImage else {
+        guard let cgImage = resizedImage.cgImage else {
             print("Trying to show an image not backed by CGImage!")
             return
         }
@@ -98,82 +98,18 @@ final class DetectorViewModel: ObservableObject {
        imageViewFramePublisher.send(imageFrame)
     }
 
-    private func scaleAndOrient(image: UIImage) -> UIImage {
-        // Set a default value for limiting image size.
-        let maxResolution: CGFloat = UIScreen.main.bounds.width
+    func resize(image: UIImage) -> UIImage? {
+        let width: Double = 640
+        let aspectScale = image.size.height / image.size.width
 
-        guard let cgImage = image.cgImage else {
-            print("UIImage has no CGImage backing it!")
-            return image
-        }
+        let resizedSize = CGSize(width: width, height: width * Double(aspectScale))
 
-        // Compute parameters for transform.
-        let width = CGFloat(cgImage.width)
-        let height = CGFloat(cgImage.height)
-        var transform = CGAffineTransform.identity
+        UIGraphicsBeginImageContextWithOptions(resizedSize, false, 0.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: resizedSize.width, height: resizedSize.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
 
-        var bounds = CGRect(x: 0, y: 0, width: width, height: height)
-
-        if width > maxResolution ||
-            height > maxResolution {
-            let ratio = width / height
-            if width > height {
-                bounds.size.width = maxResolution
-                bounds.size.height = round(maxResolution / ratio)
-            } else {
-                bounds.size.width = round(maxResolution * ratio)
-                bounds.size.height = maxResolution
-            }
-        }
-
-        let scaleRatio = bounds.size.width / width
-        let orientation = image.imageOrientation
-        switch orientation {
-        case .up:
-            transform = .identity
-        case .down:
-            transform = CGAffineTransform(translationX: width, y: height).rotated(by: .pi)
-        case .left:
-            let boundsHeight = bounds.size.height
-            bounds.size.height = bounds.size.width
-            bounds.size.width = boundsHeight
-            transform = CGAffineTransform(translationX: 0, y: width).rotated(by: 3.0 * .pi / 2.0)
-        case .right:
-            let boundsHeight = bounds.size.height
-            bounds.size.height = bounds.size.width
-            bounds.size.width = boundsHeight
-            transform = CGAffineTransform(translationX: height, y: 0).rotated(by: .pi / 2.0)
-        case .upMirrored:
-            transform = CGAffineTransform(translationX: width, y: 0).scaledBy(x: -1, y: 1)
-        case .downMirrored:
-            transform = CGAffineTransform(translationX: 0, y: height).scaledBy(x: 1, y: -1)
-        case .leftMirrored:
-            let boundsHeight = bounds.size.height
-            bounds.size.height = bounds.size.width
-            bounds.size.width = boundsHeight
-            transform = CGAffineTransform(translationX: height, y: width).scaledBy(x: -1, y: 1).rotated(by: 3.0 * .pi / 2.0)
-        case .rightMirrored:
-            let boundsHeight = bounds.size.height
-            bounds.size.height = bounds.size.width
-            bounds.size.width = boundsHeight
-            transform = CGAffineTransform(scaleX: -1, y: 1).rotated(by: .pi / 2.0)
-        @unknown default:
-                fatalError()
-        }
-
-        return UIGraphicsImageRenderer(size: bounds.size).image { rendererContext in
-            let context = rendererContext.cgContext
-
-            if orientation == .right || orientation == .left {
-                context.scaleBy(x: -scaleRatio, y: scaleRatio)
-                context.translateBy(x: -height, y: 0)
-            } else {
-                context.scaleBy(x: scaleRatio, y: -scaleRatio)
-                context.translateBy(x: 0, y: -height)
-            }
-            context.concatenate(transform)
-            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
-        }
+        return resizedImage
     }
 
     private func clearAllInfo() {
