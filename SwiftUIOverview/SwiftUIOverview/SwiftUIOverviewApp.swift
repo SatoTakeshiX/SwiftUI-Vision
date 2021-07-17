0632a6f3d90f200a2bbe4373ac570424009935a7
satoutakeshi <sato.takeshi1987@gmail.com>
2021-07-17 17:48:09 +0900

    add SwiftUIOverview

---
+++ SwiftUIOverview/SwiftUIOverview/SwiftUIOverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOverviewApp.swift
+//  SwiftUIOverview
+//
+//  Created by satoutakeshi on 2021/07/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
