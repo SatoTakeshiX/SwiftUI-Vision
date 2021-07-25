cbd64dd7af20f0ce573c080cd23fe0ebe7d1128f
satoutakeshi <sato.takeshi1987@gmail.com>
2021-07-25 17:50:27 +0900

    refactoring

--- Realtime-Face-Tracking/Realtime-Face-Tracking/View/PreviewLayerView.swift
+++ Realtime-Face-Tracking/Realtime-Face-Tracking/View/PreviewLayerView.swift
@@ -27,74 +27,37 @@ struct PreviewLayerView: UIViewControllerRepresentable {
         drawFaceObservations(detectedRect)
     }
 
-    private func radiansForDegrees(_ degrees: CGFloat) -> CGFloat {
-        return CGFloat(Double(degrees) * Double.pi / 180.0)
-    }
-
     func drawFaceObservations(_ detectedRects: [CGRect]) {
         previewLayer.sublayers?.removeSubrange(1...)
-        let captureDeviceResolution = self.pixelSize
 
         let captureDeviceBounds = CGRect(x: 0,
                                          y: 0,
-                                         width: captureDeviceResolution.width,
-                                         height: captureDeviceResolution.height)
+                                         width: pixelSize.width,
+                                         height: pixelSize.height)
 
-        let captureDeviceBoundsCenterPoint = CGPoint(x: captureDeviceBounds.midX,
-                                                     y: captureDeviceBounds.midY)
-
-        let normalizedCenterPoint = CGPoint(x: 0.5, y: 0.5)
         let overlayLayer = CALayer()
         overlayLayer.name = "DetectionOverlay"
-        overlayLayer.masksToBounds = true
-        overlayLayer.anchorPoint = normalizedCenterPoint
         overlayLayer.bounds = captureDeviceBounds
-        overlayLayer.position = captureDeviceBoundsCenterPoint//CGPoint(x: previewLayer.bounds.midX, y: previewLayer.bounds.midY)
-        overlayLayer.borderColor = UIColor.blue.cgColor
-        overlayLayer.borderWidth = 4
+        overlayLayer.position = CGPoint(x: captureDeviceBounds.midX,
+                                        y: captureDeviceBounds.midY)
 
+        print("overlay: befor: \(overlayLayer.frame)")
         let videoPreviewRect = previewLayer.layerRectConverted(fromMetadataOutputRect: CGRect(x: 0, y: 0, width: 1, height: 1))
-        var rotation: CGFloat
-        var scaleX: CGFloat
-        var scaleY: CGFloat
 
-        // Rotate the layer into screen orientation.
-        switch UIDevice.current.orientation {
-        case .portraitUpsideDown:
-            rotation = 180
-            scaleX = videoPreviewRect.width / captureDeviceResolution.width
-            scaleY = videoPreviewRect.height / captureDeviceResolution.height
-
-        case .landscapeLeft:
-            rotation = 90
-            scaleX = videoPreviewRect.height / captureDeviceResolution.width
-            scaleY = scaleX
-
-        case .landscapeRight:
-            rotation = -90
-            scaleX = videoPreviewRect.height / captureDeviceResolution.width
-            scaleY = scaleX
-
-        default:
-            rotation = 0
-            scaleX = videoPreviewRect.width / captureDeviceResolution.width
-            scaleY = videoPreviewRect.height / captureDeviceResolution.height
-        }
-
+        let (rotation, scaleX, scaleY) = makerotationAndScale(videoPreviewRect: videoPreviewRect, pixelSize: pixelSize)
         // Scale and mirror the image to ensure upright presentation.
-        let affineTransform = CGAffineTransform(rotationAngle: radiansForDegrees(rotation))
-            .scaledBy(x: scaleX, y: -scaleY)
+        let affineTransform = CGAffineTransform(rotationAngle: radiansForDegrees(rotation)).scaledBy(x: scaleX, y: -scaleY)
         overlayLayer.setAffineTransform(affineTransform)
         overlayLayer.position = CGPoint(x: previewLayer.bounds.midX, y: previewLayer.bounds.midY)
-
+        
         previewLayer.addSublayer(overlayLayer)
-
+        print("overlay: after: \(overlayLayer.frame)")
         let layers = detectedRects.compactMap { detectedRect -> CALayer in
             let xMin = detectedRect.minX
             let yMax = detectedRect.maxY
 
             var xCoord = xMin * overlayLayer.frame.size.width
-            let yCoord = (1 - yMax) * overlayLayer.frame.size.height // これがどういうことだったかをもう一度理解する
+            let yCoord = (1 - yMax) * overlayLayer.frame.size.height // layerRectConvertedで0,0よりもマイナス値でX座標が始まっていた。その分を引く
             let width = detectedRect.width * overlayLayer.frame.size.width
             let height = detectedRect.height * overlayLayer.frame.size.height
 
@@ -110,6 +73,40 @@ struct PreviewLayerView: UIViewControllerRepresentable {
         }
 
         layers.forEach { self.previewLayer.addSublayer($0) }
+    }
+
+    private func radiansForDegrees(_ degrees: CGFloat) -> CGFloat {
+        return CGFloat(Double(degrees) * Double.pi / 180.0)
+    }
+
+    private func makerotationAndScale(videoPreviewRect: CGRect, pixelSize: CGSize) -> (rotation: CGFloat, scaleX: CGFloat, scaleY: CGFloat) {
+        var rotation: CGFloat
+        var scaleX: CGFloat
+        var scaleY: CGFloat
+
+        // Rotate the layer into screen orientation.
+        switch UIDevice.current.orientation {
+        case .portraitUpsideDown:
+            rotation = 180
+            scaleX = videoPreviewRect.width / pixelSize.width
+            scaleY = videoPreviewRect.height / pixelSize.height
+
+        case .landscapeLeft:
+            rotation = 90
+            scaleX = videoPreviewRect.height / pixelSize.width
+            scaleY = scaleX
+
+        case .landscapeRight:
+            rotation = -90
+            scaleX = videoPreviewRect.height / pixelSize.width
+            scaleY = scaleX
+
+        default:
+            rotation = 0
+            scaleX = videoPreviewRect.width / pixelSize.width
+            scaleY = videoPreviewRect.height / pixelSize.height
+        }
+        return (rotation, scaleX, scaleY)
     }
 }
 //
